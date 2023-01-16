0a13964f95cf365da99cb1918e63bc6bb059f238
Emitter <xperimentschaftler@gmail.com>
2023-01-17 00:54:41 +0600

    Completed V2

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,9 @@
-import {Client, IntentsBitField, PermissionsBitField} from "discord.js";
+import {
+    Client,EmbedBuilder,
+    GuildChannel,
+    IntentsBitField,
+    PermissionsBitField
+} from "discord.js";
 import {DataTypes, Sequelize} from "sequelize";
 import * as path from "path";
 
@@ -39,7 +44,6 @@ const sequelize = new Sequelize({
 })
 
 sequelize.sync({alter: true}).then(() => {
-    console.log("Synced");
 })
 
 
@@ -48,146 +52,151 @@ client.once('ready', () => {
 })
 
 client.on('messageCreate', async msg => {
-    if (msg.author.bot) return;
+    try{
+        if (msg.author.bot) return;
+        const channelId = msg.channelId;
+        const channel = await sequelize.model("channels").findOne({
+            where: {channelId: channelId}
+        })
+        if(channel === null) return;
 
-    const channelId = msg.channelId;
-    const channel = await sequelize.model("channels").findOne({
-        where: {channelId: channelId}
-    })
-    if(channel === null) return;
+        const role = channel.get("roleId") as string;
+        if(msg.member === null) return;
+        if(msg.member.roles.cache.has(role)) return;
 
-    const role = channel.get("roleId") as string;
-    if(msg.member === null) return;
-    if(msg.member.roles.cache.has(role)) return;
+        const nextTime = (Date.now() + (channel.get("cooldown") as number * 1000) + "");
 
-    const coolDownDuration = channel.get("cooldown") as number * 1000;
-    const coolDownChannel = cooldowns.get(channelId);
-    if(!coolDownChannel){
-       cooldowns.set(channelId, new FixedSizeMap<string, number>(250));
-        cooldowns.get(channelId)?.set(msg.author.id, new Date().getTime());
-        return;
-    }
-
-    const coolDownUser = coolDownChannel.get(msg.author.id);
-
-    if(!coolDownUser){
-        cooldowns.get(channelId)?.set(msg.author.id, new Date().getTime());
-    }else{
-        let nextAllowedMessageAt = coolDownUser + coolDownDuration;
-        if((new Date().getTime()) < nextAllowedMessageAt){
-            let time = nextAllowedMessageAt.toString().substring(0, nextAllowedMessageAt.toString().length -3);
-            try{
-                msg.delete().catch(err => {});
-                const dm = await msg.member.createDM()
-                dm.send(`You can send a message in the <#${channelId}> channel again <t:${time}:R>.`).catch(err => {})
-            }catch (err) {}
-        }else{
-            cooldowns.get(channelId)?.set(msg.author.id, new Date().getTime());
-
-        }
+        await msg.member.roles.add("1062567202282287154")
+        const dm = await msg.member.createDM();
+        let s = `You will be able to chat again in the <#${channelId}> <t:${nextTime.slice(0, nextTime.length -3)}:R>, or activate "Kai Magic" to get unlimited messages on the channel and with Kai's DM (just send Kai "Go premium") 🎊🎉`;
+        const embed = new EmbedBuilder()
+            .setDescription("**" + s + "**");
+        await dm.send({embeds: [embed]})
+        saveTimeout(msg.author.id, nextTime + "")
+    }catch (err){
+        console.log(err);
     }
 })
 
 
 client.on('messageCreate', async msg => {
-    if(!msg.member?.permissions.has(PermissionsBitField.Flags.ModerateMembers)) return;
-    const args = msg.content.split(" ");
-    const commandName = args[0].toLowerCase();
-    switch (commandName){
-        case "!addcooldown":
-            const role = args[1];
-            const coolDownTime = args[2];
-            const channelId = msg.channelId;
+    try{
+        if(!msg.member?.permissions.has(PermissionsBitField.Flags.ModerateMembers)) return;
+        const args = msg.content.split(" ");
+        const commandName = args[0].toLowerCase();
+        switch (commandName){
+            case "!addcooldown":
+                const role = args[1];
+                const coolDownTime = args[2];
+                const channelId = msg.channelId;
 
-            if(!role || !coolDownTime || !channelId){
-                msg.react("⛔").catch(err => {});
-            }
-            try{
-                await channels.create({
-                    roleId: role,
-                    channelId: channelId,
-                    cooldown: coolDownTime
-                })
-                msg.react("✅").catch(err => {});
-            }catch (err){
-                msg.react("⛔").catch(err => {});
-            }
-        break;
-        case "!removecooldown":
-            const RMChannelId = msg.channelId;
-            const RMRoleId = args[1];
-
-            if(RMRoleId){
+                if(!role || !coolDownTime){
+                    msg.react("⛔").catch(err => {});
+                }
                 try{
-                    await channels.destroy({
-                        where: {
-                            channelId: RMChannelId,
-                            roleId: RMRoleId
-                        }
+                    await channels.create({
+                        roleId: role,
+                        channelId: channelId,
+                        cooldown: coolDownTime
+                    })
+                    const channel = await (msg.channel as GuildChannel).fetch();
+                    await channel.permissionOverwrites.create("1062567202282287154", {
+                        SendMessages: false
                     })
                     msg.react("✅").catch(err => {});
                 }catch (err){
                     msg.react("⛔").catch(err => {});
                 }
+                break;
+            case "!removecooldown":
+                const RMChannelId = msg.channelId;
+                const RMRoleId = args[1];
 
-            }else{
-                try{
-                    await channels.destroy({
-                        where: {
-                            channelId: RMChannelId,
-                        }
-                    })
-                    msg.react("✅").catch(err => {});
-                }catch (err){
-                    msg.react("⛔").catch(err => {});
+                if(RMRoleId){
+                    try{
+                        await channels.destroy({
+                            where: {
+                                channelId: RMChannelId,
+                                roleId: RMRoleId
+                            }
+                        })
+                        const channel =  (await msg.guild?.channels.fetch(RMChannelId)) as GuildChannel
+                        await channel.permissionOverwrites.delete("1062567202282287154");
+                        msg.react("✅").catch(err => {});
+                    }catch (err){
+                        msg.react("⛔").catch(err => {});
+                    }
+
+                }else{
+                    try{
+                        await channels.destroy({
+                            where: {
+                                channelId: RMChannelId,
+                            }
+                        })
+                        const channel = await (msg.channel as GuildChannel).fetch();
+                        await channel.permissionOverwrites.delete("1062567202282287154");
+                        msg.react("✅").catch(err => {});
+                    }catch (err){
+                        msg.react("⛔").catch(err => {});
+                    }
+
                 }
-
-            }
-
-
-
+        }
+    }catch (err){
+        console.log(err)
     }
 })
-
-
-
 
 client.login(process.env._TOKEN).then(() => {
-    console.log("Logged in");
 })
 
-export class FixedSizeMap<K, V>{
-    private readonly maxSize: number;
-    private map: Map<K, V>;
-    private keys: K[];
+const fs = require('fs');
 
-    constructor(maxSize: number) {
-        this.maxSize = maxSize;
-        this.map = new Map();
-        this.keys = [];
-    }
-
-    set(key: K, value: V): void {
-        if (this.keys.length >= this.maxSize) {
-            this.removeOldest();
-        }
-        if (!this.map.has(key)) {
-            this.keys.push(key);
-        }
-        this.map.set(key, value);
-    }
-
-    get(key: K): V | undefined {
-        return this.map.get(key);
-    }
-
-    has(key: K): boolean {
-        return this.map.has(key);
-    }
-
-    private removeOldest(): void {
-        const oldestKey = this.keys.shift();
-        this.map.delete(oldestKey as K);
+// Function to save the user's timeout information to the text file
+function saveTimeout(userId:string, timeout:string) {
+    try{
+        fs.appendFile('timeout.txt', `${userId}, ${timeout}\n`, (err: Error) => {
+        });
+    }catch (err){
+        console.log(err)
     }
 }
-const cooldowns = new FixedSizeMap<string, FixedSizeMap<string, number>>(50);
+
+// Function to read the timeout information from the text file
+const readTimeout =  () => {
+    fs.readFile('timeout.txt', 'utf8', async (err: Error, data: string) => {
+        try{
+            if (err) throw err;
+            // Split the data into separate lines
+            let lines = data.split('\n');
+            let newData = "";
+            // Iterate over each line
+            const guild = await client.guilds.fetch("859736561830592522");
+            for (let i = 0; i < lines.length; i++) {
+                // Split the line into the user ID and timeout
+                const [userId, timeout] = lines[i].split(',');
+
+                // Check if the timeout has expired
+                if (Date.now() > Number(timeout)) {
+                    // Code to unmute the user here
+                    const member = await guild.members.fetch(userId);
+                    await member.roles.remove("1062567202282287154")
+                    const dm = await member.createDM();
+                    await dm.send(`You have been unmuted and can now send messages in the channel again. Enjoy!`)
+                } else {
+                    newData += lines[i] + "\n";
+                }
+            }
+            fs.writeFile("timeout.txt", newData, (err: Error) => {
+                if (err) throw err;
+            });
+        }catch (err){
+            console.log(err);
+        }
+    });
+}
+
+
+// Call the readTimeout function every 30 seconds
+setInterval(readTimeout, 30000);
